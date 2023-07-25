8a70df85c01661717bda34412c22096dd975af55
fadimana <fdmndikici@gmail.com>
2023-07-25 12:34:25 +0300

    .

---
+++ Alıştırma2/Alıştırma2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alıştırma2
+{
+    class Program
+    {
+       public class Rectangle
+        {
+            private double width;
+            public double length;
+
+            public void Deger()
+            {
+                Console.WriteLine("Width değerini girin.");
+                width = Convert.ToInt32(Console.ReadLine());
+            }
+            public double getArea()
+            {
+                return length * width;
+            }
+            public void Display()
+            {
+                Console.WriteLine("Length:{0}", length);
+                Console.WriteLine("Width : {0}", width);
+                Console.WriteLine("Area:{0}", getArea());
+            }
+           
+        }
+        
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();//sınıfımızın nesnesini oluşturduk.
+            r.Deger();
+            r.length = 2.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
